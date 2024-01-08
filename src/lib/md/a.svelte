0d95a351cce139bcedec7402af8d9c84b80e8f8c
Chad Freeman <ceo@marinadev.xyz>
2024-01-07 19:45:47 -0500

    remove useless console.log polluting my logs

--- src/lib/md/a.svelte
+++ src/lib/md/a.svelte
@@ -1,7 +1,6 @@
 <script lang=ts>
     export let href: string;
     export let rel: HTMLLinkElement["rel"];
-    console.log(href, rel);
 </script>
 
 <a class="font-bold underline" {href} {rel}><slot /></a>
